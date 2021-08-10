bb6bfc7c29e97f3de8f1d3af57569e690af6562b
evilvegetarian <66737576+evilvegetarian@users.noreply.github.com>
2021-08-10 23:35:40 +0500

    add

--- ToDoList/Models/Date/Month.cs
+++ ToDoList/Models/Date/Month.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ToDoList.Models.Date
 {
     public class Month
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public List<Day> Days { get; set; }
     }
 }
--- ToDoList/Service/Interfaces/IApplicationService.cs
+++ ToDoList/Service/Interfaces/IApplicationService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Models.Date;
 
 namespace ToDoList.Service.Interfaces
 {
     public interface IApplicationService
     {
-        
+
     }
 }
